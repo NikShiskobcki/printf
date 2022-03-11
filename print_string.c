2d8379afa4c1fdc7ff179d622e1a9b168b94053c
PauliCrespi <4377@holbertonschool.com>
2022-03-11 15:11:13 -0800

    print string

--- print_string.c
+++ print_string.c
@@ -2,19 +2,17 @@
 
 /**
  * print_string - entry
- *
- *
+ * @s : argument for %s
+ * Return: string length
  */
 int print_string(va_list s)
 {
-  int i;
-  char *str = va_arg(s, char*s);
+	int i;
+	char *str = va_arg(s, char*s);
 
-  for (i = 0; str[i] != '\0'; i++)
-    {
-      _putchar(str[i]);
-    }
-  
-  return (i);
-  
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	return (i);
 }
