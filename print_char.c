31f770951b0e6d69c85960e72bf801471d940acd
PauliCrespi <4377@holbertonschool.com>
2022-03-11 15:07:30 -0800

    print char

--- print_char.c
+++ print_char.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- *
- *
+ *print_char - printf for char
+ *@c : argument
+ *Return: length of char
  */
 int print_char(va_list c)
 {
-  _putchar(c);
-  return(1);
+	_putchar(c);
+	return (1);
 }
