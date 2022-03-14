5e662e93d577ffc3b8929b28531d9248d8b6e374
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-14 12:21:23 -0700

    bi

--- print_bi.c
+++ print_bi.c
@@ -7,9 +7,14 @@
  */
 int print_bi(va_list b)
 {
-  unsigned long int x = va_arg(b, unsigned long int);
+  unsigned long int x;
   unsigned int a[1024];
   int i, j, cont = 0;
+
+  if (b != NULL)
+    x = va_arg(b, unsigned long int);
+  else
+    return(0);
 
   if (x == 0)
     {
