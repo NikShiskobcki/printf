08ee0551cf7cc961d718307e32cc62544321110a
PauliCrespi <4377@holbertonschool.com>
2022-03-14 13:05:10 -0700

    octal

--- print_octal.c
+++ print_octal.c
@@ -18,18 +18,19 @@ int print_octal(va_list o)
   if (x == 0)
     {
       _putchar('0');
-      return (1);
+      cont++;
     }
   
   for (i = 0; x > 0; i++)
     {
      oc[i] = x % 8;
       x = x / 8;
+      cont++;
     }
   for (j = i - 1 ; j >= 0; j--)
     _putchar(oc[j] + '0');
 
-  return (i);
+  return (cont);
 
 
 }
