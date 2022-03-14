c2ee8e948b2bf38ea6e9e5da12cae992ac9d6fdc
PauliCrespi <4377@holbertonschool.com>
2022-03-14 13:01:00 -0700

    bI

--- print_bi.c
+++ print_bi.c
@@ -7,14 +7,10 @@
  */
 int print_bi(va_list b)
 {
-  unsigned long int x;
+  unsigned long int x = va_arg(b, unsigned long int);
   unsigned int a[1024];
   int i = 0, j = 0, cont = 0;
 
-  if (b != NULL)
-    x = va_arg(b, unsigned long int);
-  else
-    return(0);
 
   if (x == 0)
     {
@@ -28,7 +24,7 @@ int print_bi(va_list b)
       x = x / 2;
       cont++;
     }
-  for (j = i; j>=0; j--)
+  for (j = i - 1; j>=0; j--)
     _putchar(a[j] + '0');
 
   return (cont);
