a0816c7e57694c4fc0785d9b94d8819f5b47afe7
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-14 10:47:15 -0700

    print string paaai

--- print_string.c
+++ print_string.c
@@ -10,9 +10,24 @@ int print_string(va_list s)
 {
 
   int i;
-  char *str = va_arg(s, char *);
+  char *str;
+
+  if (s != NULL)
+    str = va_arg(s, char *);
+
+  if (str == NULL)
+    {
+      _putchar('(');
+      _putchar('n');
+      _putchar('u');
+      _putchar('l');
+      _putchar('l');
+      _putchar(')');
+      return(6);
 
 
+
+    }
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
