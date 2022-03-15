693dd5104d473ba785874f5cd975b4a1e6447b37
PauliCrespi <4377@holbertonschool.com>
2022-03-15 08:54:47 -0700

    betty double

--- print_double.c
+++ print_double.c
@@ -3,24 +3,23 @@
 
 /**
  * print_number - Entry point
- * @n : int
+ * @x : int
  * Return: 0
  */
 
 void print_number(int x)
 {
-  unsigned int i = x;
-  if (x < 0)
-    {
-      _putchar('-');
-      i = -x;
-    }
-  if (i / 10)
-    print_number(i / 10);
-  _putchar((i % 10) + '0');
+	unsigned int i = x;
+
+	if (x < 0)
+	{
+		_putchar('-');
+		i = -x;
+	}
+	if (i / 10)
+		print_number(i / 10);
+	_putchar((i % 10) + '0');
 }
-
-
 /**
  *print_double - print double function for %d
  *@d : va list of argument of type double
@@ -28,22 +27,16 @@ void print_number(int x)
  */
 int print_double(va_list d)
 {
-  int x = va_arg(d, int);
-  int i = 1;
-  
+	int x = va_arg(d, int);
+	int i = 1;
 
 	print_number(x);
 	if (x < 0)
-	  i++;
+		i++;
 	while (x / 10 != 0)
-	  {
-	    i++;
-	    x = x / 10;
-	  }
-
+	{
+		i++;
+		x = x / 10;
+	}
 	return (i);
-
-
-
-
 }
