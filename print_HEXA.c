b9766ebb26e857e691106669cbe6ab2e774d2a3e
PauliCrespi <4377@holbertonschool.com>
2022-03-15 08:46:25 -0700

    betty HEXA

--- print_HEXA.c
+++ print_HEXA.c
@@ -6,24 +6,22 @@
  */
 int get_value(unsigned int val)
 {
-  if (val < 10)
-    return(val + '0');
-  if (val == 10)
-    return('A');
-  if (val == 11)
-    return('B');
-  if (val == 12)
-    return('C');
-  if (val == 13)
-    return('D');
-  if (val == 14)
-    return('E');
-  if (val == 15)
-    return('F');
-
-  return('x');
+	if (val < 10)
+		return (val + '0');
+	if (val == 10)
+		return ('A');
+	if (val == 11)
+		return ('B');
+	if (val == 12)
+		return ('C');
+	if (val == 13)
+		return ('D');
+	if (val == 14)
+		return ('E');
+	if (val == 15)
+		return ('F');
+	return ('x');
 }
-
 /**
  *print_HEXA - function
  *@x : va list argument
@@ -31,30 +29,29 @@ int get_value(unsigned int val)
  */
 int print_HEXA(va_list x)
 {
-  unsigned int y;
-  int arr[8];
-  int i = 0, j = 0;
-  int val;
-  int cont = 0;
-  if (x != NULL)
-    y = va_arg(x, unsigned int);
-  else
-    return (0);
+	unsigned int y;
+	int arr[8];
+	int i = 0, j = 0;
+	int val;
+	int cont = 0;
 
-  if (y == 0)
-    {
-      _putchar('0');
-      cont++;
-    }
-  for (i = 0; y > 0; i++)
-    {
-      val = y % 16;
-      arr[i] = get_value(val);
-      y = y / 16;
-      cont++;
-    }
-  for (j = i - 1; j >= 0; j--)
-    _putchar(arr[j]);
-
-  return (cont);
- }
+	if (x != NULL)
+		y = va_arg(x, unsigned int);
+	else
+		return (0);
+	if (y == 0)
+	{
+		_putchar('0');
+		cont++;
+	}
+	for (i = 0; y > 0; i++)
+	{
+		val = y % 16;
+		arr[i] = get_value(val);
+		y = y / 16;
+		cont++;
+	}
+	for (j = i - 1; j >= 0; j--)
+		_putchar(arr[j]);
+	return (cont);
+}
