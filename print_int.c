d4e20848512eba5185e0bd4736255aa42a7c7596
PauliCrespi <4377@holbertonschool.com>
2022-03-15 09:03:53 -0700

    betty int

--- print_int.c
+++ print_int.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- *print_double - print double function for %d
- *@d : va list of argument of type double
- *Return: length of double
+ *print_int - print int function for %i
+ *@i : va list of argument of type double
+ *Return: length of int
  */
 int print_int(va_list i)
 {
-  int x = va_arg(i, int);
-  int j = 1;
+	int x = va_arg(i, int);
+	int j = 1;
 
-
-  print_number(x);
-  if (x < 0)
-    j++;
-  while (x / 10 != 0)
-    {
-      j++;
-      x = x / 10;
-    }
-
-  return (j);
+	print_number(x);
+	if (x < 0)
+		j++;
+	while (x / 10 != 0)
+	{
+		j++;
+		x = x / 10;
+	}
+	return (j);
 }
