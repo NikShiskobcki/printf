9bab46566ea451f9c869da58597c38eae1d19cbd
PauliCrespi <4377@holbertonschool.com>
2022-03-15 09:12:27 -0700

    betty  string

--- print_string.c
+++ print_string.c
@@ -8,26 +8,21 @@
  */
 int print_string(va_list s)
 {
+	int i;
+	char *str;
 
-  int i;
-  char *str;
-
-  if (s != NULL)
-    str = va_arg(s, char *);
-
-  if (str == NULL)
-    {
-      _putchar('(');
-      _putchar('n');
-      _putchar('u');
-      _putchar('l');
-      _putchar('l');
-      _putchar(')');
-      return(6);
-
-
-
-    }
+	if (s != NULL)
+		str = va_arg(s, char *);
+	if (str == NULL)
+	{
+		_putchar('(');
+		_putchar('n');
+		_putchar('u');
+		_putchar('l');
+		_putchar('l');
+		_putchar(')');
+		return (6);
+	}
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
