95c41b5677a1f94755a26fe6eaaaa020613f70b6
PauliCrespi <4377@holbertonschool.com>
2022-03-15 08:50:00 -0700

    betty binary

--- print_bi.c
+++ print_bi.c
@@ -1,33 +1,27 @@
 #include "main.h"
-
 /**
- *
- *
- *
+ *print_bi - entry
+ *@b : argument
+ *Return: length
  */
 int print_bi(va_list b)
 {
-  unsigned int x = va_arg(b, unsigned long int);
-  unsigned int a[1024];
-  int i = 0, j = 0, cont = 0;
+	unsigned int x = va_arg(b, unsigned long int);
+	unsigned int a[1024];
+	int i = 0, j = 0, cont = 0;
 
-
-  if (x == 0)
-    {
-      _putchar('0');
-      cont++;
-    }
-  
-  for (i = 0; x > 0; i++)
-    {
-      a[i] = x % 2;
-      x = x / 2;
-      cont++;
-    }
-  for (j = i - 1; j>=0; j--)
-    _putchar(a[j] + '0');
-
-  return (cont);
-  
-  
+	if (x == 0)
+	{
+		_putchar('0');
+		cont++;
+	}
+	for (i = 0; x > 0; i++)
+	{
+		a[i] = x % 2;
+		x = x / 2;
+		cont++;
+	}
+	for (j = i - 1; j >= 0; j--)
+		_putchar(a[j] + '0');
+	return (cont);
 }
